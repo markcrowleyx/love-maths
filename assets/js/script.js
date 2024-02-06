0bc45f451e9ef29ecf07165c8ce2c23e9eb1fd25
markcrowleyx <markcrowleyx@gmail.com>
2024-02-06 08:22:52 +0000

    Add function checkAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -41,7 +41,22 @@ function checkAnswer() {
 
 }
 
+
+/**Gets the operands (the numbers) and the operator (plus, minus, etc)
+ * directly from the DOM and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === '+') {
+        return [operand1 + operand2, 'addition'];
+    } else {
+        alert(`unimplemented operator ${operator}`);
+        throw `unimplemented operator ${operator}. Aborting`;
+    }
 
 }
 
